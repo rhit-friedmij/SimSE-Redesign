1397a1148dd514ec03a6f6b940e0ec34047591a2
jurgenkr <jurgenkr@rose-hulman.edu>
2023-02-07 12:36:08 -0500

    logic class done

--- src/simse/codegenerator/logicgenerator/LogicGenerator.java
+++ src/simse/codegenerator/logicgenerator/LogicGenerator.java
@@ -21,55 +21,38 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
+import javax.lang.model.element.Modifier;
 import javax.swing.JOptionPane;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.JavaFile;
+import com.squareup.javapoet.MethodSpec;
+import com.squareup.javapoet.TypeSpec;
 
 public class LogicGenerator implements CodeGeneratorConstants {
   private MiscUpdaterGenerator miscUGen; // generates MiscUpdater component
-  private TriggerCheckerGenerator trigGen; // generates trigger checker
-                                           // component
-  private DestroyerCheckerGenerator destGen; // generates destroyer checker
-                                             // component
-  private MenuInputManagerGenerator menuGen; // generates menu input manager
-                                             // component
-  private EmployeeParticipantSelectionDialogGenerator epsdGen; // generates
-                                                               // employee
-                                                               // participant
-                                                               // selection
-                                                               // dialogs
-  private NonEmployeeParticipantSelectionDialogGenerator nepsdGen; // generates
-                                                                   // non-employee
-                                                                   // participant
-                                                                   // selection
-                                                                   // dialogs
-  private ParticipantSelectionDialogsDriverGenerator psdDriverGen; // generates
-                                                                   // participant
-                                                                   // selection
-                                                                   // dialogs
-                                                                   // driver
-  private ChooseActionToDestroyDialogGenerator catddGen; // generates choose
-                                                         // action to destroy
-                                                         // dialog
-  private ChooseActionToJoinDialogGenerator catjdGen; // generates choose action
-                                                      // to join dialog
-  private ChooseRoleToPlayDialogGenerator crtpdGen; // generates choose role to
-                                                    // play dialog
+  private TriggerCheckerGenerator trigGen; // generates trigger checker component
+  private DestroyerCheckerGenerator destGen; // generates destroyer checker component
+  private MenuInputManagerGenerator menuGen; // generates menu input manager component
+  private EmployeeParticipantSelectionDialogGenerator epsdGen; // generates employee participant selection dialogs
+  private NonEmployeeParticipantSelectionDialogGenerator nepsdGen; // generates non-employee participant selection dialogs
+  private ParticipantSelectionDialogsDriverGenerator psdDriverGen; // generates participant selection dialogs driver
+  private ChooseActionToDestroyDialogGenerator catddGen; // generates choose action to destroy dialog
+  private ChooseActionToJoinDialogGenerator catjdGen; // generates choose action to join dialog
+  private ChooseRoleToPlayDialogGenerator crtpdGen; // generates choose role to play dialog
   private RuleExecutorGenerator ruleGen; // generates rule executor component
   private File directory; // directory to generate into
+  private File logicFile; // file to generate
 
-  public LogicGenerator(ModelOptions options, DefinedObjectTypes objTypes,
-      DefinedActionTypes actTypes) {
+  public LogicGenerator(ModelOptions options, DefinedObjectTypes objTypes, DefinedActionTypes actTypes) {
     directory = options.getCodeGenerationDestinationDirectory();
     miscUGen = new MiscUpdaterGenerator(directory, actTypes);
     trigGen = new TriggerCheckerGenerator(actTypes, directory);
     destGen = new DestroyerCheckerGenerator(actTypes, directory);
-    menuGen = new MenuInputManagerGenerator(options, actTypes, objTypes, 
-        directory);
-    epsdGen = new EmployeeParticipantSelectionDialogGenerator(actTypes,
-        objTypes, directory);
-    nepsdGen = new NonEmployeeParticipantSelectionDialogGenerator(actTypes,
-        objTypes, directory);
-    psdDriverGen = new ParticipantSelectionDialogsDriverGenerator(actTypes,
-        directory);
+    menuGen = new MenuInputManagerGenerator(options, actTypes, objTypes, directory);
+    epsdGen = new EmployeeParticipantSelectionDialogGenerator(actTypes, objTypes, directory);
+    nepsdGen = new NonEmployeeParticipantSelectionDialogGenerator(actTypes, objTypes, directory);
+    psdDriverGen = new ParticipantSelectionDialogsDriverGenerator(actTypes, directory);
     catddGen = new ChooseActionToDestroyDialogGenerator(actTypes, directory);
     catjdGen = new ChooseActionToJoinDialogGenerator(actTypes, directory);
     crtpdGen = new ChooseRoleToPlayDialogGenerator(actTypes, directory);
@@ -93,122 +76,88 @@ public class LogicGenerator implements CodeGeneratorConstants {
     crtpdGen.generate();
     boolean success = ruleGen.generate();
 
+    ClassName stage = ClassName.get("javafx.stage", "Stage");
+    ClassName state = ClassName.get("simse.state", "State");
+    ClassName menuInput = ClassName.get("simse.logic", "MenuInputManager");
+    ClassName trigCheck = ClassName.get("simse.logic", "TriggerChecker");
+    ClassName destCheck = ClassName.get("simse.logic", "DestroyerChecker");
+    ClassName ruleExecuter = ClassName.get("simse.logic", "RuleExecuter");
+    ClassName miscUpdater = ClassName.get("simse.logic", "MiscUpdater");
+    
+    MethodSpec logicConstructor = MethodSpec.constructorBuilder()
+			.addModifiers(Modifier.PUBLIC)
+			.addParameter(state, "s")
+			.addStatement("state = s")
+			.addStatement("updater = new MiscUpdater(state)")
+			.addStatement("ruleEx = new RuleExecutor(state)")
+			.addStatement("trigChecker = new TriggerChecker(state, ruleEx)")
+			.addStatement("destChecker = new DestroyerChecker(state, ruleEx, trigChecker)")
+			.addStatement("ruleEx.setTriggerChecker(trigChecker)")
+			.addStatement("ruleEx.setDestroyerChecker(destChecker)")
+			.addStatement("menInputMgr = new MenuInputManager(state, trigChecker, destChecker, ruleEx)")
+			.build();
+    
+    MethodSpec menu = MethodSpec.methodBuilder("getMenuInputManager")
+			.addModifiers(Modifier.PUBLIC)
+			.returns(menuInput)
+			.addStatement("return menInputMgr")
+			.build();
+    
+    MethodSpec trigger = MethodSpec.methodBuilder("getTriggerChecker")
+			.addModifiers(Modifier.PUBLIC)
+			.returns(trigCheck)
+			.addStatement("return trigChecker")
+			.build();
+    
+    MethodSpec destroyer = MethodSpec.methodBuilder("getDestroyerChecker")
+			.addModifiers(Modifier.PUBLIC)
+			.returns(destCheck)
+			.addStatement("return destChecker")
+			.build();
+    
+    MethodSpec update = MethodSpec.methodBuilder("update")
+			.addModifiers(Modifier.PUBLIC)
+			.returns(void.class)
+			.addParameter(stage, "mainGUI")
+			.addStatement("updater.update()")
+			.addStatement("trigChecker.update(false, mainGUI)")
+			.addStatement("ruleEx.update(mainGUI, $T.UPDATE_ALL_CONTINUOUS, null, null)", ruleExecuter)
+			.addStatement("destChecker.update(false, mainGUI)")
+			.build();
+    
+    TypeSpec logic = TypeSpec.classBuilder("Logic")
+			.addModifiers(Modifier.PUBLIC)
+			.addField(state, "state", Modifier.PRIVATE)
+			.addField(miscUpdater, "updater", Modifier.PRIVATE)
+			.addField(trigCheck, "trigChecker", Modifier.PRIVATE)
+			.addField(destCheck, "destChecker", Modifier.PRIVATE)
+			.addField(menuInput, "menInputMgr", Modifier.PRIVATE)
+			.addField(ruleExecuter, "ruleEx", Modifier.PRIVATE)
+			.addMethod(logicConstructor)
+			.addMethod(menu)
+			.addMethod(trigger)
+			.addMethod(destroyer)
+			.addMethod(update)
+			.build();
+    
+    JavaFile javaFile = JavaFile.builder("simse.logic", logic)
+			.addFileComment("File generated by: simse.codegenerator.logicgenerator.LogicGenerator")
+			.build();
+    
     // generate outer logic component:
-    File logicFile = new File(directory, ("simse\\logic\\Logic.java"));
-    if (logicFile.exists()) {
-      logicFile.delete(); // delete old version of file
-    }
     try {
-      FileWriter writer = new FileWriter(logicFile);
-      writer
-          .write("/* File generated by: simse.codegenerator.logicgenerator.LogicGenerator */");
-      writer.write(NEWLINE);
-      writer.write("package simse.logic;");
-      writer.write(NEWLINE);
-      writer.write("import javax.swing.*;");
-      writer.write(NEWLINE);
-      writer.write("import simse.state.*;");
-      writer.write(NEWLINE);
-      writer.write("public class Logic");
-      writer.write(NEWLINE);
-      writer.write(OPEN_BRACK);
-      writer.write(NEWLINE);
-
-      // member variables:
-      writer.write("private State state;");
-      writer.write(NEWLINE);
-      writer.write("private MiscUpdater updater;");
-      writer.write(NEWLINE);
-      writer.write("private TriggerChecker trigChecker;");
-      writer.write(NEWLINE);
-      writer.write("private DestroyerChecker destChecker;");
-      writer.write(NEWLINE);
-      writer.write("private MenuInputManager menInputMgr;");
-      writer.write(NEWLINE);
-      writer.write("private RuleExecutor ruleEx;");
-      writer.write(NEWLINE);
-
-      // constructor:
-      writer.write("public Logic(State s)");
-      writer.write(NEWLINE);
-      writer.write(OPEN_BRACK);
-      writer.write(NEWLINE);
-      writer.write("state = s;");
-      writer.write(NEWLINE);
-      writer.write("updater = new MiscUpdater(state);");
-      writer.write(NEWLINE);
-      writer.write("ruleEx = new RuleExecutor(state);");
-      writer.write(NEWLINE);
-      writer.write("trigChecker = new TriggerChecker(state, ruleEx);");
-      writer.write(NEWLINE);
-      writer
-          .write("destChecker = new DestroyerChecker(state, ruleEx, trigChecker);");
-      writer.write(NEWLINE);
-      writer.write("ruleEx.setTriggerChecker(trigChecker);");
-      writer.write(NEWLINE);
-      writer.write("ruleEx.setDestroyerChecker(destChecker);");
-      writer.write(NEWLINE);
-      writer
-          .write("menInputMgr = new MenuInputManager(state, trigChecker, destChecker, ruleEx);");
-      writer.write(NEWLINE);
-      writer.write(CLOSED_BRACK);
-      writer.write(NEWLINE);
-
-      // methods:
-
-      // getMenuInputManager() method:
-      writer.write("public MenuInputManager getMenuInputManager()");
-      writer.write(NEWLINE);
-      writer.write(OPEN_BRACK);
-      writer.write(NEWLINE);
-      writer.write("return menInputMgr;");
-      writer.write(NEWLINE);
-      writer.write(CLOSED_BRACK);
-      writer.write(NEWLINE);
-
-      // getTriggerChecker() method:
-      writer.write("public TriggerChecker getTriggerChecker()");
-      writer.write(NEWLINE);
-      writer.write(OPEN_BRACK);
-      writer.write(NEWLINE);
-      writer.write("return trigChecker;");
-      writer.write(NEWLINE);
-      writer.write(CLOSED_BRACK);
-      writer.write(NEWLINE);
-      
-      // getDestroyerChecker() method:
-      writer.write("public DestroyerChecker getDestroyerChecker() {");
-      writer.write(NEWLINE);
-      writer.write("return destChecker;");
-      writer.write(NEWLINE);
-      writer.write(CLOSED_BRACK);
-      writer.write(NEWLINE);
-
-      // update() method:
-      writer.write("public void update(JFrame mainGUI)");
-      writer.write(NEWLINE);
-      writer.write(OPEN_BRACK);
-      writer.write(NEWLINE);
-      writer.write("updater.update();");
-      writer.write(NEWLINE);
-      writer.write("trigChecker.update(false, mainGUI);");
-      writer.write(NEWLINE);
-      writer
-          .write("ruleEx.update(mainGUI, RuleExecutor.UPDATE_ALL_CONTINUOUS, null, null);");
-      writer.write(NEWLINE);
-      writer.write("destChecker.update(false, mainGUI);");
-      writer.write(NEWLINE);
-      writer.write(CLOSED_BRACK);
-      writer.write(NEWLINE);
-
-      writer.write(CLOSED_BRACK);
-      writer.close();
-      return success;
+    	logicFile = new File(directory, ("simse\\logic\\Logic.java"));
+        if (logicFile.exists()) {
+          logicFile.delete(); // delete old version of file
+        }
+        
+        System.out.println(javaFile.toString());
+		javaFile.writeTo(logicFile);
+		return success;
     } catch (IOException e) {
-      JOptionPane.showMessageDialog(null, ("Error writing file "
-          + logicFile.getPath() + ": " + e.toString()), "File IO Error",
-          JOptionPane.WARNING_MESSAGE);
-      return false;
+    	JOptionPane.showMessageDialog(null, ("Error writing file " + logicFile.getPath() + ": " + e.toString()), 
+    		  "File IO Error", JOptionPane.WARNING_MESSAGE);
+    	return false;
     }
   }
 }
--- src/test/generation/TestGenerator.java
+++ src/test/generation/TestGenerator.java
@@ -5,8 +5,12 @@ import java.util.Vector;
 
 import simse.codegenerator.enginegenerator.EngineGenerator;
 import simse.codegenerator.enginegenerator.StartingNarrativeDialogGenerator;
+import simse.codegenerator.logicgenerator.LogicGenerator;
 import simse.modelbuilder.ModelOptions;
+import simse.modelbuilder.actionbuilder.ActionType;
+import simse.modelbuilder.actionbuilder.DefinedActionTypes;
 import simse.modelbuilder.objectbuilder.Attribute;
+import simse.modelbuilder.objectbuilder.DefinedObjectTypes;
 import simse.modelbuilder.objectbuilder.NonNumericalAttribute;
 import simse.modelbuilder.objectbuilder.SimSEObjectType;
 import simse.modelbuilder.startstatebuilder.CreatedObjects;
@@ -18,29 +22,53 @@ public class TestGenerator {
 	private static File directory = new File("C:\\Users\\jurgenkr\\Documents\\GitHub\\SeniorProject\\CodeGenTest");
 	private static CreatedObjects cObjs = new CreatedObjects();
 	private static ModelOptions options = new ModelOptions();
+	private static DefinedObjectTypes objTypes = new DefinedObjectTypes();
+	private static DefinedActionTypes actTypes = new DefinedActionTypes();
 	
 	public static void main(String[] args) {
+		setUp();
+//		startingNarrativeDialogTest();
+//		engineTest();
+		logicTest();
+	}
+	
+	public static void setUp() {
 		cObjs.setStartingNarrative("     Welcome to SimSE! Your task is to create Groceries@Home, a Web-based system that will allow people to place orders over the Internet for groceries to be delivered to their homes. The customer is the Grocery Home Delivery Service, a company who, up until now, has taken orders for groceries solely by telephone, but now wants to step into the information age. \n     Your budget is $280,000, and you have 1,350 clock ticks to complete the project. However, you should keep checking your project info to monitor this information -- the customer has the tendency to introduce new requirements, and will sometimes give you more time and/or money along with those new requirements. \n     Your final score will be out of 100 points, and it will be calculated based on how complete and error-free your code is, whether your code is integrated or not, and how well you stick to your budget and schedule. \n\nTwo notes:\n* Each hired employee is always paid every clock tick regardless of whether they're busy or not. So use them wisely!\n* If you want to use a tool that you have purchased in a task, you must specify that when you assign the task. If you have already started the task without the tool, you must stop and restart the task with the tool if you want it to be used.\n\nGood luck!");
-//		StartingNarrativeDialogGenerator sndg = new StartingNarrativeDialogGenerator(cObjs, directory);
-//		sndg.generate();
 		
 		options.setCodeGenerationDestinationDirectory(directory);
 		NonNumericalAttribute name = new NonNumericalAttribute("name", 1, true, true, true);
 		InstantiatedAttribute ia1 = new InstantiatedAttribute(name, "Gomez");
 		Vector<InstantiatedAttribute> v1 = new Vector<>();
 		v1.add(ia1);
-		SimSEObjectType t1 = new SimSEObjectType(1, "Obsessed");
-		t1.addAttribute(name);
-		cObjs.addObject(new SimSEObject(v1, t1));
+		SimSEObjectType ot1 = new SimSEObjectType(1, "Obsessed");
+		ot1.addAttribute(name);
+		cObjs.addObject(new SimSEObject(v1, ot1));
+		objTypes.addObjectType(ot1);
 		
 		InstantiatedAttribute ia2 = new InstantiatedAttribute(name, "Wednesday");
 		Vector<InstantiatedAttribute> v2 = new Vector<>();
 		v2.add(ia2);
-		SimSEObjectType t2 = new SimSEObjectType(1, "Unobsessed");
-		t2.addAttribute(name);
-		cObjs.addObject(new SimSEObject(v2, t2));
+		SimSEObjectType ot2 = new SimSEObjectType(1, "Unobsessed");
+		ot2.addAttribute(name);
+		cObjs.addObject(new SimSEObject(v2, ot2));
+		objTypes.addObjectType(ot2);
+		
+		ActionType at1 = new ActionType("Cultivate");
+		actTypes.addActionType(at1);
+	}
+	
+	public static void startingNarrativeDialogTest() {
+		StartingNarrativeDialogGenerator sndg = new StartingNarrativeDialogGenerator(cObjs, directory);
+		sndg.generate();
+	}
+	
+	public static void engineTest() {
 		EngineGenerator eGen = new EngineGenerator(options, cObjs);
 		eGen.generate();
 	}
-
+	
+	public static void logicTest() {
+		LogicGenerator lGen = new LogicGenerator(options, objTypes, actTypes);
+		lGen.generate();
+	}
 }
