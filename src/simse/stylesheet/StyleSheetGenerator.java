5831f5e9735672f4a888577bdfd24db09df0c43b
thedasherx <75217270+rhit-webbma@users.noreply.github.com>
2023-02-14 16:11:41 -0500

    Stylesheet generator created

---
+++ src/simse/stylesheet/StyleSheetGenerator.java
@@ -0,0 +1,52 @@
+package simse.stylesheet;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class StyleSheetGenerator {
+
+	private File dir;
+	
+	public StyleSheetGenerator(File dir) {
+		this.dir = dir;
+	}
+
+	private void generate() {
+		// TODO Auto-generated method stub
+	    File stylesheetFile = new File(dir,
+	            ("animations\\styles.css"));
+	    
+	        if (stylesheetFile.exists()) {
+	        	stylesheetFile.delete(); // delete old version of file
+	        }
+		
+		
+		FileReader reader = null;
+		FileWriter writer = null;
+		try {
+			reader = new FileReader("C:\\Users\\localmgr\\git\\simse-redesign\\simse-redesign\\res\\static\\stylesheet\\staticstylesheet.txt");
+			writer = new FileWriter(stylesheetFile);
+
+		
+	
+		String fileContents = "";
+		int index;
+		
+		while ((index = reader.read()) != -1) {
+			fileContents += (char)index;
+		}
+		writer.write(fileContents);
+		
+		reader.close();
+		writer.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
